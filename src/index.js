d4ccb8e40429df9826a261dd11082c2374a97955
Justin Rex <justirex@amazon.com>
2024-04-12 15:26:11 -0700

    duplicate audio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactPlayer from 'react-player';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ReactPlayer
+    width="100vw"
+    height="100dvh"
+    url="https://videos.ctfassets.net/cpcn7rmzq5g6/3dv5AmEoul0vAwC7ts3IIP/30453c5c51f6914a485252f9b9547acc/4_8__We_Need_to_Talk__Son_Teases_Mom_About_All_Her_Shopping_STILL_3.mp4"
+    controls={true}
+    playing={true}
+  />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
